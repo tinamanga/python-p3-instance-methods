e4bdd27eb0670c0e645673316763fceb180b8e84
tinamanga <tinamanga@gmail.com>
2025-05-13 11:25:09 +0300

    Describe an instance method. Call instance methods on an object. Build instance methods for an object.

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 class Dog:
-    # Class body goes here
+ def bark(self):
+    print("Woof!")
 
-    #Instance method definition
-    pass
+ #Instance method definition
+ def sit(self):
+    print("The dog is sitting.")
+
+    
